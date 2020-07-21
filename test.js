b78c16218c6f8341a4a00da2d2ab2e999539290e
hellanoone <68422793+hellanoone@users.noreply.github.com>
2020-07-21 20:08:32 +0300

    Update test.js

--- test.js
+++ test.js
@@ -2,16 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(process.env.TOKEN, {polling: true});
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-
-    const chatId = msg.chat.id;
-    const resp = match[1];
-
-    bot.sendMessage(chatId, resp);
-  });
-
-  bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-
-    bot.sendMessage(chatId, 'Маем кантакт');
-  });
+bot.on(text = 'message', msg => {
+    const {chat: { id }} = msg
+    bot.sendMessage(id, 'You\'ve entered: ' + text)
+})
