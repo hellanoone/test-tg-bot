986ae988e27eeb5bd8027238b3f3af36347074cf
hellanoone <68422793+hellanoone@users.noreply.github.com>
2020-07-22 10:44:26 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,34 +1,34 @@
-const TelegramBot = require('node-telegram-bot-api');
+// const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new TelegramBot('1215534715:AAHLyPzwuF56Xd1yN9UwQzaNNQNnZo5PL9g', {polling: true});
+// const bot = new TelegramBot('1215534715:AAHLyPzwuF56Xd1yN9UwQzaNNQNnZo5PL9g', {polling: true});
 
-bot.on('message', msg => {
-    const {chat: { id }} = msg
-    const regRasp = '/^\d{1,}(\+|-|\*|\/|%|^)\d{1,}$/'
-    var text = msg.text
+// bot.on('message', msg => {
+//     const {chat: { id }} = msg
+//     const regRasp = '/^\d{1,}(\+|-|\*|\/|%|^)\d{1,}$/'
+//     var text = msg.text
 
-    if (regRasp.test(text)) {
-        var str = text
-        let arr = str.split(' ')
-        let sign = arr[1]
-        let val1 = Number(arr[0])
-        let val2 = Number(arr[2])
+//     if (regRasp.test(text)) {
+//         var str = text
+//         let arr = str.split(' ')
+//         let sign = arr[1]
+//         let val1 = Number(arr[0])
+//         let val2 = Number(arr[2])
 
-        switch (sign) {
-            case '+':
-                a = val1+val2
-            case '-':
-                a = val1-val2
-            case '*':
-                a = val1*val2
-            case '/':
-                a = val1/val2
-            case '%':
-                a = val1%val2
-            case '^':
-                a = val1**val2
-        }
-    }
+//         switch (sign) {
+//             case '+':
+//                 a = val1+val2
+//             case '-':
+//                 a = val1-val2
+//             case '*':
+//                 a = val1*val2
+//             case '/':
+//                 a = val1/val2
+//             case '%':
+//                 a = val1%val2
+//             case '^':
+//                 a = val1**val2
+//         }
+//     }
 
-    bot.sendMessage(id, 'The result is: ' + a)
-})
+//     bot.sendMessage(id, 'The result is: ' + a)
+// })
