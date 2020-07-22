084e012c926e4ae4fde664759fad9ad4e45d3755
hellanoone <68422793+hellanoone@users.noreply.github.com>
2020-07-22 10:27:39 +0300

    Update test.js

--- test.js
+++ test.js
@@ -3,11 +3,32 @@ const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot('1215534715:AAHLyPzwuF56Xd1yN9UwQzaNNQNnZo5PL9g', {polling: true});
 
 bot.on('message', msg => {
-    
-    const {chat: { id }} = msg
-//    var text = msg.text
-//    bot.sendMessage(id, 'You\'ve entered: ' + text)
 
-    if (msg.text.toString() === /^\d{1,}(+|-|*|/|%|^)\d{1,}$/) {
-        bot.sendMessage(id,"Right");
+    const {chat: { id }} = msg
+    var text = msg.text
+    var rasp = '/^\d{1,}(\+|-|\*|\/|%|^)\d{1,}$/'
+    //if (rasp.match() === text) {
+        var str = text
+        let arr = str.split(' ')
+        let sign = arr[1]
+        let val1 = Number(arr[0])
+        let val2 = Number(arr[2])
+
+        switch (sign) {
+            case '+':
+                a = val1+val2
+            case '-':
+                a = val1-val2
+            case '*':
+                a = val1*val2
+            case '/':
+                a = val1/val2
+            case '%':
+                a = val1%val2
+            case '^':
+                a = val1**val2
+        }
+    //}
+
+    bot.sendMessage(id, 'The result is: ' + a)
 })
