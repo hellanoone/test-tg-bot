280debd186a46c1d05e6564083f0e1a5ffa6c9c4
hellanoone <68422793+hellanoone@users.noreply.github.com>
2020-07-22 10:41:25 +0300

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot('1215534715:AAHLyPzwuF56Xd1yN9UwQzaNNQNnZo5PL9g', {polling: true});
 
-bot.comm('message', msg => {
+bot.on('message', msg => {
     const {chat: { id }} = msg
     const regRasp = '/^\d{1,}(\+|-|\*|\/|%|^)\d{1,}$/'
     var text = msg.text
