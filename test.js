3be34f2a884b5872a67938aca9c69ff21ce56649
hellanoone <68422793+hellanoone@users.noreply.github.com>
2020-07-22 09:54:58 +0300

    Update test.js

--- test.js
+++ test.js
@@ -5,8 +5,8 @@ const bot = new TelegramBot('1215534715:AAHLyPzwuF56Xd1yN9UwQzaNNQNnZo5PL9g', {p
 bot.on('message', msg => {
     
     const {chat: { id }} = msg
-    var text = msg.text
-    bot.sendMessage(id, 'You\'ve entered: ' + text)
+//    var text = msg.text
+//    bot.sendMessage(id, 'You\'ve entered: ' + text)
 
     if (msg.text.toString() === /^\d{1,}(+|-|*|/|%|^)\d{1,}$/) {
         bot.sendMessage(id,"Right");
