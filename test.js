e4de9b74ad9a3fb14dfdac559d34babbb7050df8
hellanoone <68422793+hellanoone@users.noreply.github.com>
2020-07-22 09:51:29 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new TelegramBot(process.env.TOKEN, {polling: true});
+const bot = new TelegramBot('1215534715:AAHLyPzwuF56Xd1yN9UwQzaNNQNnZo5PL9g', {polling: true});
 
-bot.on(text = 'message', msg => {
+bot.on('message', msg => {
+    
     const {chat: { id }} = msg
+    var text = msg.text
     bot.sendMessage(id, 'You\'ve entered: ' + text)
+
+    var rasp = /^\d{1,}(+|-|*|/|%|^)\d{1,}$/
+    if (msg.text.toString() === rasp) {
+        bot.sendMessage(msg.chat.id,"Right");
 })
